94f59dba9d3c50890c5818bf1145dfaf616a00fd
creationari <86951351+creationari@users.noreply.github.com>
2023-01-17 19:07:55 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,43 @@
+Status = "";
+bedroom_image = "";
+objects = [];
+
+function preload(){
+    fan_image = loadImage("bedroom.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(bedroom_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(bedroom_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("red");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+    }
+}
